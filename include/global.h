171ece762321525a19b024a2768c03abd7a1d411
Andreas Habel <virus-d@fcms.de>
2011-05-23 21:46:46 +0200

    cleanup

--- include/global.h
+++ include/global.h
@@ -1,22 +1,24 @@
-
-
+// Color definitions for cli output
 #define NORMAL         "\033[0m"
 #define BOLD			  "\033[1m"
 #define COLOR_WARNING  "\033[0;33m" //yellow
 #define COLOR_ERROR    "\033[0;31m" //red
 #define COLOR_FATAL    "\033[1;31m" //red bold
 
-
-//
 // fDebug Config default settings
-//
 #define DEFAULT_PORT   			 5005
 #define FDEBUG_VERSION        "0.3.1"
 
+// fDebug Message Types
 #define FDEBUG_CONTROL     1
 #define FDEBUG_MESSAGE     2
-#define FDEBUG_WARNING     4
-#define FDEBUG_ERROR       8
-#define FDEBUG_FATAL       16
-#define FDEBUG_VARIABLES   32
-#define FDEBUG_SOURCE      64
+#define FDEBUG_WARNING     3
+#define FDEBUG_ERROR       4
+#define FDEBUG_FATAL       5
+#define FDEBUG_VARIABLES   6
+#define FDEBUG_SOURCE      7
+
+// proxy specific message types
+#define FDEBUG_REGISTER    8
+#define FDEBUG_SETCLIENT   9
+
